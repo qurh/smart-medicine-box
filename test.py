c170928a31b8315710107e3b682e6b317a4fcc7c
屈瑞航 <quruihang.happy@gmail.com>
2025-07-21 09:55:25 +0800

    文件整理

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_community.llms import Tongyi
-
-os.environ["DASHSCOPE_API_KEY"] = 'sk-7b93e8e814104fcd9051de56186c190e'
-llm = Tongyi(model_name="qwen-plus", temperature=0.7)
-# text = llm.invoke("请给我写一句情人节红玫瑰的中文宣传语")
-text = llm.invoke("请给我的花店起个名")
-print(text)
